8da7cc51c4f1aa26c87ba082246d179db230490a
qianyulei <3472647653@qq.com>
2026-05-13 14:51:22 +0800

    feat: 初步完成顶部导航条

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,46 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from "@/components/Navigation.vue";
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <el-row :gutter="10">
+          <el-col :span="4" class="title">
+            BiliCourse网课管理系统
+          </el-col>
+          <el-col :span="10">
+            <navigation></navigation>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.el-row {
+  margin-bottom: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.el-row:last-child {
+  margin-bottom: 0;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+.el-col {
+  border-radius: 4px;
+  //background-color: grey;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.title {
+  line-height: 60px;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <el-menu class="el-menu-demo" mode="horizontal">
+    <el-menu-item index="1">课程列表</el-menu-item>
+    <el-menu-item index="2">学习笔记</el-menu-item>
+    <el-menu-item index="3">思维导图</el-menu-item>
+  </el-menu>
+</template>
+
+<style scoped>
+
+</style>
